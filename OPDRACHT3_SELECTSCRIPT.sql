8f65dbd757d61dfd6e4898e6f1e5a6f36a27944b
Patrick Roelofs <stolenbows@gmail.com>
2019-12-18 20:57:16 +0100

    Opdracht M verplaatst naar hoofdbestand

--- OPDRACHT3_SELECTSCRIPT.sql
+++ OPDRACHT3_SELECTSCRIPT.sql
@@ -157,19 +157,49 @@ GO
 /* M */
 /* Gebruikers [mail_adress] en het gemiddelde aantal films die elke gebruiker per dag kijkt. Toon alleen gebruikers die gemiddeld 2 of meer films per dag kijken, met het grootste gemiddelde bovenaan. */
 
+/* pas een customer aan zodat de datum calculatie beter is */
+UPDATE Customer SET Customer.subscription_start = '2018-DEC-01' WHERE Customer.customer_mail_address = 'Lorem.ipsum@Curabiturconsequat.edu'
+SELECT * from customer where customer_mail_address = 'Lorem.ipsum@Curabiturconsequat.edu'
+
+/* Test om te zien hoeveel deze persoon gekeken heeft */
+SELECT Customer.customer_mail_address, count(movie_id) AS [movies watched]
+FROM WatchHistory, customer WHERE WatchHistory.customer_mail_address = customer.customer_mail_address AND Customer.customer_mail_address = 'Lorem.ipsum@Curabiturconsequat.edu'
+GROUP BY customer.customer_mail_address
+ORDER BY [movies watched] DESC
+
+/* test om te zien hoeveel dagen deze persoon klant is */
+SELECT Customer.customer_mail_address, DATEDIFF(DAY, Customer.subscription_start, dbo.dateCalc(Customer.subscription_end, convert(date, GETDATE()))) AS [days the person has an account]
+FROM WatchHistory, Customer
+WHERE WatchHistory.customer_mail_address = Customer.customer_mail_address AND Customer.customer_mail_address = 'Lorem.ipsum@Curabiturconsequat.edu'
+GROUP BY Customer.customer_mail_address, Customer.subscription_start, Customer.subscription_end
+ORDER BY [days the person has an account] DESC
+
+/* De echte berekening */
 DROP VIEW IF EXISTS average_time
 GO
 
+DROP FUNCTION IF EXISTS dbo.dateCalc
+GO
+
+CREATE FUNCTION dbo.dateCalc(@endDate DATE, @today DATE)
+RETURNS DATE
+    AS
+    BEGIN
+        IF(@endDate IS NULL)
+            RETURN @today
+        ELSE IF(@endDate < @today)
+            RETURN @endDate
+        RETURN @today
+    END
+GO
+
 CREATE VIEW average_time AS
-    SELECT WatchHistory.customer_mail_address, count(watch_date) as [average movies watched in a day]
-    FROM WatchHistory, Customer
-    WHERE WatchHistory.customer_mail_address = Customer.customer_mail_address
-    GROUP BY WatchHistory.customer_mail_address, WatchHistory.watch_date
+	SELECT WatchHistory.customer_mail_address, count(movie_id) / datediff(DAY, Customer.subscription_start, dbo.dateCalc(Customer.subscription_end, convert(date, GETDATE()))) as [gemiddelde aantal films]
+	FROM WatchHistory, Customer WHERE WatchHistory.customer_mail_address = Customer.customer_mail_address -- AND Customer.customer_mail_address = 'Lorem.ipsum@Curabiturconsequat.edu'
+	GROUP BY WatchHistory.customer_mail_address, Customer.subscription_start, Customer.subscription_end
+    HAVING count(movie_id) / datediff(DAY, Customer.subscription_start, dbo.dateCalc(Customer.subscription_end, convert(date, GETDATE()))) >= 2
 GO
 
 SELECT *
 FROM average_time
-ORDER BY [average movies watched in a day] DESC
-
-SELECT * FROM WatchHistory
-SELECT * FROM Movie
+ORDER BY [gemiddelde aantal films] DESC
